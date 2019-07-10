1f2d3d947f10053c4809cf9a0d8cac8a807c69e4
SwitchJS <dev@switchjs.com>
2019-07-09 21:43:12 -0700

    0.2.0: object info

--- NOTES.md
+++ NOTES.md
@@ -114,3 +114,67 @@ Scrolling speed of first segment stored in low 2 bits at offset 0x0E.
 
 Scrolling speed for subsequent segments are stored in the object offset 0x0E in
 bits 2-3 (mask 0x0C)
+
+## Objects
+
+The level area has storage for exactly 2600 Objects starting at address `0x248`.
+Each Object is of size `0x20`
+
+### Object Position and Size
+
+X-Coordinate: 32-bit unsigned integer at offset `0x00`
+
+Y-Coordinate: 32-bit unsigned integer at offset `0x04`
+
+The position appears to be the centerpoint of the object assuming the grid is
+160 units wide and 160 units tall.  For example, a `1 x 1` block positioned at
+the bottom of the grid has Y-coordinate `80` rather than `0`.
+
+Width: 8-bit unsigned integer at offset `0x0A`
+
+Height: 8-bit unsigned integer at offset `0x0B`
+
+The sizes are specified in tiles.  For example, AutoScroll waypoints are 3 tiles
+wide and 1 tile tall.
+
+### Object Type
+
+16-bit unsigned integer at offset `0x18`
+
+#### Items
+
+| Value | M1 Obj Type    | Common Modifiers             |
+|------:|:---------------|------------------------------|
+| `0x08`| Coin           | Wing, Parachute              |
+| `0x14`| Super Mushroom | Wing, Parachute              |
+| `0x21`| 1-Up Mushroom  | Wing, Parachute              |
+| `0x22`| Fire Flower    | Wing, Parachute, Mushroom !! |
+| `0x23`| Super Star     | Wing, Parachute              |
+| `0x2C`| Big Mushroom   | Wing, Parachute, Mushroom    |
+| `0x2D`| Shoe Goomba    | Wing, Parachute, Mushroom !! |
+| `0x46`| 10-Coin        | Wing, Parachute, !!          |
+| `0x5C`| Pink Coin      | (none)                       |
+
+#### Miscellaneous
+
+| Value | Description          |
+|------:|:---------------------|
+| `0x59`| Auto-Scroll Waypoint |
+
+### Object Flags
+
+32-bit unsigned integer at offset `0x0C`.
+
+| BitMask | Interpretation    |
+|--------:|:------------------|
+| `1<< 1` | Wings             |
+| `1<<15` | Parachute         |
+| `1<<18` | Mushroom Modifier |
+
+Part-specific modifiers:
+
+| BitMask | Part        | Interpretation                |
+|--------:|:------------|:------------------------------|
+| `1<< 2` | Fire Flower | Superball Flower              |
+| `1<< 2` | Shoe Goomba | Stiletto                      |
+| `3<<18` | 10-Coin     | Value: 0 = 10, 1 = 30, 2 = 50 |
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*! smm2data (C) SwitchJS */
-const smm2data = (function() {
+const smm2data = (() => {
+  const OBJECT_COUNT = 2600;
   const CourseThemeNames = [
     "Ground",
     "Underground",
@@ -17,31 +18,85 @@ const smm2data = (function() {
   const MotionNames = [ "None", "OneWay", "TwoWay" ];
   const CCCatNames  = [ "None", "Parts", "Status", "Action" ];
 
+  /* utilities to read values from Uint8Array / Buffer */
   const UInt16ValAt = (a, idx) => ( a[idx] | (a[idx+1]<<8) ) >>> 0;
   const UInt32ValAt = (a, idx) => ( a[idx] | (a[idx+1]<<8) | (a[idx+2]<<16) | (a[idx+3]<<24) ) >>> 0;
   const ASCIIString = (buf) => Array.from({length: buf.length}, (_, i) => String.fromCharCode(buf[i])).join("");
   const UCS2String  = (buf) => Array.from({length: buf.length/2}, (_, i) => String.fromCharCode(buf[2*i] + (buf[2*i+1]<<8))).join("");
   const UCS2StringTrimmed = (buf) => UCS2String(buf).replace(/\x00.*$/,"");
 
-  function parse_liquid(buf) {
-    return {
-      Final: buf[0x204],
-      Start: buf[0x207],
-      Motion: MotionNames[buf[0x205]] || "??",
-      Speed: SpeedNames[buf[0x206]] || "??"
-    }
-  }
+  /* parse water/lava properties */
+  const parse_liquid = (buf) => ({
+    Final: buf[0x204],
+    Start: buf[0x207],
+    Motion: MotionNames[buf[0x205]] || "??",
+    Speed: SpeedNames[buf[0x206]] || "??"
+  });
 
-  function parse_clear_cond(buf) {
+  /* clear condition */
+  const parse_clear_cond = (buf) => {
     const o = {
       Category: CCCatNames[buf[0x0F]]||"??",
       Type: UInt32ValAt(buf, 0x10).toString(16)
     };
     if(o.Category == "Parts") o.Value = UInt16ValAt(buf, 0x06);
     return o;
-  }
+  };
 
-  function parse_course_data(buf) {
+  /* get 32-byte object */
+  const get_raw_object = (buf, i) => {
+    const o = buf.slice(0x248 + 0x20 * i, 0x248 + 0x20 * i + 0x20);
+    return (o instanceof ArrayBuffer) ? new Uint8Array(o) : o;
+  };
+
+  const parse_auto_scroll = (buf) => {
+    const o = {
+      Points: [],
+      Speeds: []
+    };
+
+    o.Speeds.push(buf[0x0E] & 0x03);
+
+    //const obj_cnt = UInt32ValAt(buf, 0x21C);
+    for(let i = 0; i < OBJECT_COUNT /* obj_cnt */; ++i) {
+      const obj_buf = get_raw_object(buf, i);
+      if(UInt16ValAt(obj_buf, 0x18) != 0x59) continue;
+      o.Speeds.push((obj_buf[0x0E] & 0x0C)>>2);
+    }
+    return o;
+  };
+
+  const parse_objects = (buf) => {
+    const o = [];
+    //const obj_cnt = UInt32ValAt(buf, 0x21C);
+    for(let i = 0; i < OBJECT_COUNT /* obj_cnt */; ++i) {
+      const obj_buf = get_raw_object(buf, i);
+      let chk = 0;
+      for(let j = 0; j < 0x20; ++j) chk |= obj_buf[j];
+      if(chk == 0) continue;
+      const obj = {
+        //data: Buffer(obj_buf),
+        Type: UInt16ValAt(obj_buf, 0x18),
+        //RawX: UInt32ValAt(obj_buf, 0x00),
+        //RawY: UInt32ValAt(obj_buf, 0x04),
+        Width:  obj_buf[0x0A],
+        Height: obj_buf[0x0B],
+        RawFlags: UInt32ValAt(obj_buf, 0x0C)
+      };
+      if((obj.RawFlags>>1) & 1) obj.Wings = true;
+      if((obj.RawFlags>>15) & 1) obj.Parachute = true;
+      obj.TileX = (UInt32ValAt(obj_buf, 0x00) - obj.Width * 80) / 160;
+      obj.TileY = (UInt32ValAt(obj_buf, 0x04) - obj.Height * 80) / 160;
+      //obj.RawFlags &= ~(1<<1);
+      //obj.RawFlags &= ~(1<<15);
+      //obj.RawFlags &= ~(1<<18);
+      //obj.RawFlags &= ~(1<<2);
+      o.push(obj);
+    }
+    return o;
+  };
+
+  const parse_course_data = (buf) => {
     const o = {};
     o.Info = {};
     o.Info.Name = UCS2StringTrimmed(buf.slice(0xF4, 0x134));
@@ -57,33 +112,26 @@ const smm2data = (function() {
       o.Info.AutoScroll = SpeedNames[buf[0x201]];
       if(o.Info.AutoScroll == "Custom") o.Info.ASData = parse_auto_scroll(buf);
     }
+    o.Objects = parse_objects(buf);
     return o;
-  }
+  };
 
-  function get_raw_object(buf, i) {
-    const o = buf.slice(0x248 + 0x20 * i, 0x248 + 0x20 * i + 0x20);
-    return (o instanceof ArrayBuffer) ? new Uint8Array(o) : o;
-  }
-
-  function parse_auto_scroll(buf) {
-    const o = {
-      Points: [],
-      Speeds: []
-    };
-
-    o.Speeds.push(buf[0x0E] & 0x03);
-
-    const obj_cnt = UInt32ValAt(buf, 0x21C);
-    for(let i = 0; i < obj_cnt; ++i) {
-      const obj_buf = get_raw_object(buf, i);
-      if(UInt16ValAt(obj_buf, 0x18) != 0x59) continue;
-
-      o.Speeds.push((obj_buf[0x0E] & 0x0C)>>2);
-    }
-    return o;
-  }
+  const ObjectTypeNames = {};
+  ObjectTypeNames["M1"] = {
+    0x08: "Coin",
+    0x14: "Super Mushroom",
+    0x21: "1-Up Mushroom",
+    0x22: "Fire Flower",
+    0x23: "Super Star",
+    0x2C: "Big Mushroom",
+    0x2D: "Shoe Goomba",
+    0x46: "10-Coin",
+    0x5C: "Pink Coin"
+  };
+  const get_object_name = (id, style) => ((ObjectTypeNames[style] || {})[id] || ObjectTypeNames["M1"][id] || id);
 
   return {
+    get_object_name,
     parse_course_data
   };
 })();
